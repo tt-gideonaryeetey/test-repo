432ad8570556898dc616bd3cec9431b6b814978a
Gideon Aryeetey <gideon.aryeetey@turntabl.io>
2023-05-05 08:37:30 +0000

    test implementation

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    private int number;
+    private String name;
+
+
+    public Person(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
